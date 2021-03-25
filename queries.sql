b98c44b8dfc59ca123afaedd11d3f0f7c7c20017
Gerardo Delgado <gdelgado100@gmail.com>
2021-03-25 16:15:04 -0500

    Assignment 4, Part 1 complete

--- queries.sql
+++ queries.sql
@@ -1,7 +1,10 @@
-## Part 1: Test it with SQL
+--## Part 1: Test it with SQL
+--id (integer, primary key), employer (varchar), name (varchar), skills (varchar)
 
-## Part 2: Test it with SQL
+-- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location = "St. Louis City";
 
-## Part 3: Test it with SQL
+-- Part 3: Test it with SQL
+DROP TABLE job;
 
-## Part 4: Test it with SQL
+-- Part 4: Test it with SQL
